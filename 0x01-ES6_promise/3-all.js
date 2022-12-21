7b2ca8f1b964f0371dc18b4f4bf96a51e6db40f1
1xcodex <yishak.ab1216@gmail.com>
2022-12-21 21:07:30 +0300

    edited

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,10 @@
-/* Handle multiple successful promises
-    Put () because return Promise resolve object
-*/
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((values) => {
-      const { body } = values[0];
-      const { firstName, lastName } = values[1];
-
-      console.log(`${body} ${firstName} ${lastName}`);
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
-
-export default handleProfileSignup;
